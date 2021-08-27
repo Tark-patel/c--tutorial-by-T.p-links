b57c3c912358c990c897034d66e7e1cc53c24385
Tark-patel <84756407+Tark-patel@users.noreply.github.com>
2021-08-27 20:30:45 +0530

    Create day_two_data_sucture.c

---
+++ day_two_data_sucture.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a ; ///int stands for integer 
+float b ;/// float is uesed for storing decimal value 
+char c; // char is used for storing charecter
+printf("The value of integer A = %d\n",a);
+printf("The value of float b = %f\n",b);
+printf("The value of char c = %d\n",c);
+return 0;
+}
